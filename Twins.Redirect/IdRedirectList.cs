c90fdc34c849614172b11e1bf6eeb5733584e201
Jesper Hauge <jesper@iqit.dk>
2014-11-19 23:57:02 +0100

    Project created, idRedirects coded

---
+++ Twins.Redirect/IdRedirectList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Web.Caching;
+using System.Xml.Linq;
+using log4net;
+
+namespace Twins.Redirect
+{
+    public class IdRedirectList
+    {
+        private const string CacheName = "Twins.Redirect.IdRedirects";
+
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private readonly Dictionary<int, string> _redirects;
+
+        public IdRedirectList(string idListPath)
+        {
+            IsInitialized = false;
+            var cache = HttpContext.Current.Cache;
+
+            // Read from cache if possible
+            if (cache[CacheName] != null)
+            {
+                Log.Debug("Read redirectlist from cache");
+                _redirects = (Dictionary<int, string>) cache[CacheName];
+                if (_redirects != null)
+                {
+                    IsInitialized = true;
+                    return;
+                };
+            }
+
+            // Read from file
+            if (!File.Exists(idListPath)) return;
+            var xdoc = XDocument.Load(idListPath);
+            if (xdoc.Root == null) return;
+            var listElement = xdoc.Root.Element("IdRedirects");
+            if (listElement == null) return;
+            _redirects = new Dictionary<int, string>();
+            foreach (XElement el in listElement.Elements("Redirect"))
+            {
+                int id;
+                if (!int.TryParse(el.Attribute("id").Value, out id)) continue;
+                string location = el.Attribute("location").Value;
+                if (!string.IsNullOrWhiteSpace(location))
+                {
+                    try
+                    {
+                        _redirects.Add(id, location);
+                    }
+                    catch (ArgumentException)
+                    {
+                    }
+                }
+            }
+
+            // Add to cache
+            var cacheDependency = new CacheDependency(idListPath);
+            cache.Insert(CacheName, _redirects, cacheDependency);
+
+            // Finish up
+            IsInitialized = true;
+        }
+
+        public bool IsInitialized { get; private set; }
+
+        public string GetLocation(int id)
+        {
+            try
+            {
+                return _redirects[id];
+            }
+            catch (KeyNotFoundException)
+            {
+                return "404";
+            }
+        }
+    }
+}
---
+++ Twins.Redirect/Modules/RedirectModule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web;
+using log4net;
+using Umbraco.Core;
+
+namespace Twins.Redirect.Modules
+{
+    public class RedirectModule : IHttpModule
+    {
+        private const string RedirectFilepath = "/config/TwinsRedirects.config";
+
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            Log.DisposeIfDisposable();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            Log.Info("Initializing redirects");
+            context.BeginRequest += OnBeginRequest;
+        }
+
+        #endregion
+
+        public void OnLogRequest(Object source, EventArgs e)
+        {
+            //custom logging logic can go here
+        }
+
+        private void OnBeginRequest(object sender, EventArgs e)
+        {
+            HttpContext context = HttpContext.Current;
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+            string matchStr = request.Url.PathAndQuery;
+
+            // Redirect id=1234 urls
+            var idRgex = new Regex(@"^\/(default\.asp\?|\?)id=\d*$", RegexOptions.IgnoreCase);
+            var redirectList = new IdRedirectList(context.Server.MapPath(RedirectFilepath));
+            if (idRgex.IsMatch(matchStr) && redirectList.IsInitialized)
+            {
+                var idStr = matchStr.Substring(matchStr.LastIndexOf('=') + 1);
+                int id;
+                if (int.TryParse(idStr, out id))
+                {
+                    var location = redirectList.GetLocation(id);
+                    Log.Debug(string.Format("Redirecting url id: {0} to {1}", id, location));
+                    if (location != "404")
+                    {
+                        Create301Response(location, ref response);
+                    }
+                    response.End();
+                }
+            }
+        }
+
+        private static void Create301Response(string location, ref HttpResponse response)
+        {
+            response.StatusCode = (int) HttpStatusCode.MovedPermanently;
+            response.RedirectLocation = location;
+        }
+    }
+}
